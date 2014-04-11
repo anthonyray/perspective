0b448b25db875551b0274f3829cd612280b1d6d8
Anthony Reinette <anthonyreinette@MacBook-Air-de-Anthony.local>
2014-04-12 00:58:56 +0200

    Refactored code and added resume/stop shape detection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,65 +8,109 @@ navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.msGetUserMedia;
 
 /*
-* 
+* Variables
 */
+var referenceColor = { red : 65 , green : 173 , blue : 73};
+var detection=true;
+
+/*
+* DOM Setup
+*/
+
 var video = document.getElementById('webcam');
 var canvas = document.getElementById('canvas');
 var resultCanvas = document.getElementById('resultCanvas');
 var ctx = canvas.getContext('2d');
 var resultCtx = resultCanvas.getContext('2d');
 var grid = null;
-var referenceColor = { red : 54 , green : 126 , blue : 79};
+
 /*
-* Function that will be executed 
+* Binding DOM Events with functions
+*/
+$("#detect").click(function(){
+  detection = true;
+});
+
+$("#stopdetect").click(stopDetectingShape);
+
+/*
+* Function that will be executed
 */
 
 function tick(){
 
-	requestAnimationFrame(tick); // Recursive call
+  requestAnimationFrame(tick); // Recursive call to make a loop
+  if (detection){
+    if( video.readyState === video.HAVE_ENOUGH_DATA ) { // Make sure there is something to be captured
 
-	if(video.readyState === video.HAVE_ENOUGH_DATA) { // Make sure there is something to be captured
-		var canvasWidth = canvas.width;
-		var canvasHeight = canvas.height;
-		
-		ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight); // Draw the image to the (invisible) canvas
-		var imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight); // Get the ImageData from the canvas
+      var canvasWidth = canvas.width;
+      var canvasHeight = canvas.height;
 
-		/*
-		* Image processing
-		*/
-		
-		grid = new Grid(imageData,30,30);
-		grid.process();
-		//grid.fillGridMeanCellColor();
-		var points = grid.getFourPoints(); // Get the points 
+      ctx.drawImage(video, 0, 0, canvasWidth, canvasHeight); // Draw the image to the (invisible) canvas
+      var imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight); // Get the ImageData from the canvas
 
-		/*
-		* Draw the result on the visible Canvas
-		*/
+      /*
+      * Image processing
+      */
 
-		//$("#info").text( "Distance : " + Math.floor(grid.data[0].distance) + ", R : " + Math.floor(grid.data[0].red) + " , G : " + Math.floor(grid.data[0].green) + ", B : " + Math.floor(grid.data[0].blue) );
-		$('#info').html( 'X : '+ points[0].X + ' Y: ' + points[0].Y + ' dist : ' + points[0].distance + '<br>' +
-		 'X : '+ points[1].X + ' Y: ' + points[1].Y + ' dist : ' + points[1].distance + "<br>" +
-		 "X : "+ points[2].X + " Y: " + points[2].Y + " dist : " + points[2].distance + "<br>" +
-		 "X : "+ points[3].X + " Y: " + points[3].Y + " dist : " + points[3].distance + "<br>");
-		/*grid.fillCell(points[0].X,points[0].Y,255,0,0);
-		grid.fillCell(points[1].X,points[1].Y,255,0,0);
-		grid.fillCell(points[2].X,points[2].Y,255,0,0);
-		grid.fillCell(points[3].X,points[3].Y,255,0,0);*/
+      grid = new Grid(imageData,20,20);
+      grid.process();
+      var points = grid.getFourPoints(); // Get the points
 
-		// Draw the shape of the paper detected
-		resultCtx.putImageData(imageData,0,0);
-		resultCtx.strokeStyle = "green";
-		resultCtx.lineWidth = 5;
-		resultCtx.beginPath();
-		resultCtx.moveTo(points[0].X , points[0].Y );
-		resultCtx.lineTo(points[1].X , points[1].Y );
-		resultCtx.lineTo(points[3].X , points[3].Y );
-		resultCtx.lineTo(points[2].X , points[2].Y );
-		resultCtx.closePath();
-		resultCtx.stroke();
-	}
+      /*
+      * Draw the result on the visible Canvas
+      */
+      //Draw the image
+      resultCtx.putImageData(imageData,0,0);
+
+      // Divide by the canvas in 4 quadrants
+      divideCanvas(resultCtx);
+
+      // Draw the shape of the detected paper
+      drawShape(resultCtx,points);
+    }
+  }
+
+}
+
+function divideCanvas(context) {
+  context.strokeStyle = "yellow";
+  context.lineWidth = 2 ;
+  context.beginPath();
+  context.moveTo(grid.width / 2,0);
+  context.lineTo(grid.width / 2 , grid.height );
+  context.closePath();
+  context.stroke();
+  context.strokeStyle = "yellow";
+  context.lineWidth = 2 ;
+  context.beginPath();
+  context.moveTo(0,grid.height / 2);
+  context.lineTo(grid.width , grid.height / 2 );
+  context.closePath();
+  context.stroke();
+}
+
+function drawShape(context,points){
+  context.strokeStyle = "green";
+  context.lineWidth = 3;
+  context.beginPath();
+  context.moveTo(points[0].X , points[0].Y );
+  context.lineTo(points[1].X , points[1].Y );
+  context.lineTo(points[3].X , points[3].Y );
+  context.lineTo(points[2].X , points[2].Y );
+  context.closePath();
+  context.stroke();
+}
+
+function stopDetectingShape(){
+  if (detection)
+    detection = false;
+  else
+    alert("The detection hasn't started");
+}
+
+function detectShape() {
+  requestAnimationFrame(tick);
 }
 
 if (navigator.getUserMedia) { // Make sure the browser has a getUserMedia capability
@@ -75,16 +119,12 @@ if (navigator.getUserMedia) { // Make sure the browser has a getUserMedia capabi
 
 	function(stream) {
 		video.src = window.URL.createObjectURL(stream); // creates an URL object from the stream and pass it to the video element
+    timeoutId = setTimeout( detectShape, 1000);
 	}
 	,function(){
 		alert("Failed getting the stream");
 	});
 
-	setTimeout(function(){
-		requestAnimationFrame(tick);
-	},1000);
-
 } else {
   alert("This website has a too high level of awesomeness for your browser, please upgrade it");
 }
-                       
